26b049d26de9833ebb37b3d3d24ce5522eb8e870
blinkday <zyj@zhangyangjindeMacBook-Air.local>
2016-10-25 14:41:39 +0800

    add oneTest2

---
+++ src/test/java/oneTest2.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by zyj on 16/10/22.
+ */
+public class oneTest2 {
+    @Test
+    public void test1(){
+        System.out.println("yes!");
+        assertEquals("1","1");
+    }
+
+}
